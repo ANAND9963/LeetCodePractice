64f947d41b8bd26a241b34331643f3061db13164
Anand Kumar <89594667+ANAND9963@users.noreply.github.com>
2025-06-09 12:18:24 -0400

    Time: 140 ms (8.15%) | Memory: 41.5 MB (38.42%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,41 +1,45 @@
 class Solution {
-    private static final int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    
-    public int getMaximumGold(int[][] grid) {
+    private static final int[][] directions = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+    private int maxGold = 0;
+    private List<Integer> bestPath = new ArrayList<>();
 
-        int m = grid.length;
-        int n = grid[0].length;
-        int maxGold = 0;
-        ;
+    public int getMaximumGold(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] != 0) {
-                    maxGold = Math.max(maxGold, dfs(grid, i, j));
+                    List<Integer> currentPath = new ArrayList<>();
+                    dfs(grid, i, j, 0, currentPath);
                 }
             }
         }
+
+        System.out.println("Best path: " + bestPath);
         return maxGold;
     }
 
-    private int dfs(int[][] grid, int x, int y) {
-
+    private void dfs(int[][] grid, int x, int y, int currentSum, List<Integer> path) {
         if (x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] == 0) {
-            return 0;
+            return;
         }
 
-        int originalGold = grid[x][y];
-        grid[x][y] = 0;
+        int gold = grid[x][y];
+        currentSum += gold;
+        path.add(gold);
+        grid[x][y] = 0; 
 
-        int maxGoldFromHere = 0;
+        if (currentSum > maxGold) {
+            maxGold = currentSum;
+            bestPath = new ArrayList<>(path);
+        }
 
         for (int[] dir : directions) {
-            int nx = x + dir[0];
-            int ny = y + dir[1];
-            maxGoldFromHere = Math.max(maxGoldFromHere, dfs(grid, nx, ny));
+            dfs(grid, x + dir[0], y + dir[1], currentSum, path);
         }
 
-        grid[x][y] = originalGold;
-        return originalGold + maxGoldFromHere;
+        grid[x][y] = gold; 
+        path.remove(path.size() - 1); 
     }
 
 }
