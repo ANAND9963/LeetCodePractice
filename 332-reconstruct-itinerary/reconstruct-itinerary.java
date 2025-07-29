01febe3063f733816be39733122b0571e8cd285f
Anand Kumar <89594667+ANAND9963@users.noreply.github.com>
2025-07-29 12:25:27 -0500

    Time: 5 ms (99.69%) | Memory: 45.3 MB (79.61%) - LeetSync

---
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> findItinerary(List<List<String>> tickets) {
+
+        Map<String , PriorityQueue<String>> map = new HashMap<>();
+
+        for(List<String> ticket : tickets){
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+
+            if(!map.containsKey(from)){
+                map.put(from, new PriorityQueue<String>());
+            }
+
+            map.get(from).add(to);
+        }
+
+        List<String> result = new ArrayList<>();
+
+        dfs("JFK", map , result);
+
+         Collections.reverse(result);
+        return result;
+    }
+
+    private void dfs(String airport ,Map<String , PriorityQueue<String>> map , List<String> result ){
+            PriorityQueue<String> destinations = map.get(airport);
+
+            while(destinations != null && !destinations.isEmpty()){
+                dfs(destinations.poll(),map, result);
+            }
+
+            result.add(airport);
+        
+    }
+}
